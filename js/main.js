ab338beff7353a9377623443417c2272351850ab
Roman-creat-web <htmlcursed@gmail.com>
2021-09-10 17:29:34 +0300

    сделал меню бургер

--- js/main.js
+++ js/main.js
@@ -1,5 +1,89 @@
+$(function () {
+	$('.menu__button').on('click', function () {
+		$('.menu, .menu__button').toggleClass('active');
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // $(function () {
 // 	$('.menu__button').on('click', function () {
 // 		$('.menu').toggleClass('menu--active');
 // 	});
 // });
+
